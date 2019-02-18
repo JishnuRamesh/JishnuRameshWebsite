da46ac01018989ed717b6fe80d01d95c013045ed
Jishnu <jish_r@live.com>
2019-02-18 17:27:56 +1100

    Added about me info

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,8 +8,13 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Pages>
     <SEO title="About" />
-    <h1>Hi from the about page</h1>
-    <p>Welcome to page 2</p>
+    <h1>ABOUT ME </h1>
+    <p>I am an aspiring software developer. I mostly code in Python and Javascript </p>
+    <p> Currently i'm pursuing my Master's in Information Technology at Monash Univeristy.
+    I completed my undergraduete in Electronics and Telecommunication Engineering from Kerala Univeristy in India</p>
+    <p> My favorite stack right now js React.Js on the frontend and ASP.Net in the backend. I have also worked with Java, Mongo DB, Django</p>
+    <p> Previously i used to work as a software test engineer as a contract employee for Intel Co-operation</p>
+    <p> During my off time i love to watch football. I am an avid Manchster United Fan</p>
     <Link to="/">Go back to the homepage</Link>
  
   </Pages>
