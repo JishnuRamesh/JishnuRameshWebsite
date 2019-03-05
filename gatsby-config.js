73c530bc9ca63536581dfbe41a7985641bf574fa
Jishnu Ramesh <Jish_r@live.com>
2019-03-05 19:29:31 +1100

    chnaged theme color to black

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         short_name: `jishnu-ramesh`,
         start_url: `/`,
         background_color: `#fa8017`,
-        theme_color: `#fa8017`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
